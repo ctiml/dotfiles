8ef1708ace3c174f9d9a4694d783e3c4506399f6
Timothy Lee <timmy.ccl@gmail.com>
2013-11-01 19:12:04 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+cd ~
+ln -s ~/work/dotfiles/.bashrc .bashrc
+ln -s ~/work/dotfiles/.bash_profile .bash_profile
+ln -s ~/work/dotfiles/.gitconfig .gitconfig
+ln -s ~/work/dotfiles/.vimrc .vimrc
+ln -s ~/work/dotfiles/.vim .vim
